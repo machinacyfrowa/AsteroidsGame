0bddd3e3abaa251584441c5a7fd27b3335c181c1
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-02-12 10:06:11 +0100

    input driven movement working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,19 @@ public class PlayerController : MonoBehaviour
     {
         //dodaj do wspó³rzêdnych wartoœæ x=1, y=0, z=0 pomno¿one przez czas
         //mierzony w sekundach od ostatniej klatki
-        transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+
+        //prezentacja dzia³ania wyg³adzonego sterowania (emualcja joystika)
+        //Debug.Log(Input.GetAxis("Vertical"));
+
+        //sterowanie prêdkoœci¹
+        //stworz nowy wektor przesuniêcia o wartoœci 1 do przodu
+        Vector3 movement = Vector3.forward;
+        //pomnó¿ go przez czas od ostatniej klatki
+        movement *= Time.deltaTime;
+        //pomnó¿ go przez "wychylenie joystika"
+        movement *= Input.GetAxis("Vertical");
+        //dodaj ruch do obiektu
+        transform.position += movement;
     }
 }
