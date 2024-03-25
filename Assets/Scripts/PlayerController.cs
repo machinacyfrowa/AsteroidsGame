1dfd9feb631d5b45a2a2b258a0c60abb86e0c697
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-03-25 09:52:02 +0100

    Generate  level target and ortate nav marker to it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float rotationSpeed = 100f;
     public float flySpeed = 5f;
+    //odniesienie do menadzera poziomu
+    GameObject levelManagerObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelManagerObject = GameObject.Find("LevelManager");
     }
 
     // Update is called once per frame
@@ -53,7 +55,17 @@ public class PlayerController : MonoBehaviour
         //dodaj obrót do obiektu
         //nie możemy użyć += ponieważ unity używa Quaternionów do zapisu rotacji
         transform.Rotate(rotation);
+        UpdateUI();
+    }
 
+    private void UpdateUI()
+    {
+        //metoda wykonuje wszystko związane z aktualizacją interfejsu użytkownika
+
+        //wyciagnij z menadzera poziomu pozycje wyjscia
+        Vector3 target = levelManagerObject.GetComponent<LevelManager>().exitPosition;
+        //obroc znacznik w strone wyjscia
+        transform.Find("NavUI").Find("TargetMarker").LookAt(target);
     }
 
     private void OnCollisionEnter(Collision collision)
